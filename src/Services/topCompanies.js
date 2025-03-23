fae4ec5618a5fbd941608aba9977ac55c989145f
Sira Satheesh <satheesh2254@gmail.com>
2025-03-23 23:40:26 +0530

    backend

--- src/Services/topCompanies.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-const BASE_URL = "http://localhost:3000/companies";
-
-export const getTopCompaniesList = async () => {
-  const response = await axios.get(BASE_URL);
-  response.data.sort((a, b) => b.rating - a.rating);
-  return response.data.slice(0, 6);
-}
