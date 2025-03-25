b3481db4d4b1a659654a524ce3f509c4cb0c27d9
Sira Satheesh <satheesh2254@gmail.com>
2025-03-25 15:53:24 +0530

    BackEnd removed

---
+++ src/Services/users.js
@@ -0,0 +1,76 @@
+import axios from "axios";
+
+const BASE_URL = "http://localhost:3000/users";
+
+export const getUsers = async () => {
+  const response = await axios.get(BASE_URL);
+  return response.data;
+};
+export const getUser = async (id) => {
+  const response = await axios.get(`${BASE_URL}/${id}`);
+  return response.data;
+};
+
+export const addUser = async (user) => {
+  await axios.post(BASE_URL, user);
+};
+
+export const updateUser = async (id, data) => {
+  await axios.put(`${BASE_URL}/${id}`, data);
+};
+
+
+export const getUserId = async (email) =>
+{
+  // const response = await getUsers()
+  // return response.find(user => user.email === email).id
+  const response = await axios(`${BASE_URL}/userId/${email}`)
+  return response.data.userId;
+}
+
+
+
+export const checkUserExists = async (email) => {
+  // const users = await getUsers();
+  // return users.some(user => user.email === email);
+  const response = await axios.get(`${BASE_URL}/exists/${email}`);
+  return response.data.exists;
+};
+
+export const checkCredentials = async (email,password) => {
+  // const users = await getUsers();
+  // return users.some(user => (user.email === email) && (user.password === password));
+  const response= await axios.post(`${BASE_URL}/check-credentials`, {
+    email,
+    password
+})
+return response.data.valid
+};
+
+export const guestLogin =async (defaultData={
+  name: "Guest User",
+  email: "guest@example.com",
+  method: "guest",
+}) => {
+  try {
+    const response = await axios.get(BASE_URL);
+    const users = response.data;
+
+    const existingUser = users.find(user => user.email === "guest@example.com");
+
+    if (existingUser) {
+      const updatedUser = await axios.put(`${BASE_URL}/${existingUser.id}`, defaultData);
+      return updatedUser;
+    } else {
+      const newGuest = {
+        id: Date.now().toString(),
+        ...defaultData
+      };
+      const createdUser = await axios.post(BASE_URL, newGuest);
+      return true
+    }
+  } catch (error) {
+    console.error("Error handling guest login:", error);
+    return false
+  }
+}
